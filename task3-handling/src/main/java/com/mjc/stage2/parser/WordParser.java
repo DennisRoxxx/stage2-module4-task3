732264b755a45cc8407676c6a8441de5d4ac1d37
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2023-02-22 10:06:28 +0300

    done

--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,8 +1,18 @@
 package com.mjc.stage2.parser;
 
 
-public class WordParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
 
-    // Write your code here!
+public class WordParser extends AbstractTextParser {
+
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        char[] words = string.toCharArray();
+        for (char element : words) {
+            abstractTextComponent.add(new SymbolLeaf(element, TextComponentType.WORD));
+        }
+    }
 
 }
